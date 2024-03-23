8989009532f5aeef17612f0363f5b491b51d97b5
amoore <amoore@brocksolutions.com>
2024-03-23 20:24:45 +0000

    Added inputs

--- wingspanscorecard/src/app/page.tsx
+++ wingspanscorecard/src/app/page.tsx
@@ -1,6 +1,10 @@
 import React from "react"
-
+import Form from '@/app/ui/create-form'
 
 export default function Page() {
-  return <div>Hello World</div>;  
+  return (
+    <div className="flex w-1/3 p-8">
+      <Form />
+    </div>
+  );
 }
