800f4759c3d768244709865665c7b9008cabadeb
amoore <amoore@brocksolutions.com>
2024-03-25 22:05:20 +0000

    Added server functionality for changing the number of players

--- wingspanscorecard/src/app/lib/server-uploads.tsx
+++ wingspanscorecard/src/app/lib/server-uploads.tsx
@@ -6,95 +6,77 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { safeParseInt, safeParseBool } from '@/app/lib/utils';
 
-const FormSchema = z.object({
-    id: z.number(),
-    date: z.string(),
-    time: z.string(),
-    european_expansion: z.boolean(),
-    oceania_expansion: z.boolean(),
-    asian_expansion: z.boolean(),
-    p1_id: z.string(),
-    p1_bird_points: z.number(),
-    p1_bonus_cards: z.number(),
-    p1_end_of_round_goals: z.number(),
-    p1_eggs: z.number(),
-    p1_food_on_cards: z.number(),
-    p1_tucked_cards: z.number(),
-    p1_nectar: z.number(),
-    p2_id: z.string(),
-    p2_bird_points: z.number(),
-    p2_bonus_cards: z.number(),
-    p2_end_of_round_goals: z.number(),
-    p2_eggs: z.number(),
-    p2_food_on_cards: z.number(),
-    p2_tucked_cards: z.number(),
-    p2_nectar: z.number(),
-});
+export async function addGame(formData:FormData) {
+    const numPlayers = safeParseInt(formData.get('numplayers'));
+    
+    const GameDataSchema = z.object({
+        id: z.number(),
+        date: z.string(),
+        time: z.string(),
+        european_expansion: z.boolean(),
+        oceania_expansion: z.boolean(),
+        asian_expansion: z.boolean(),
+    });
 
-const CreateGame = FormSchema.omit({id: true})
+    const AddGame = GameDataSchema.omit({id: true});
 
-export async function createGame(formData:FormData) {
     const {
         date,
         time,
         european_expansion,
         oceania_expansion,
         asian_expansion,
-        p1_id,
-        p1_bird_points,
-        p1_bonus_cards,
-        p1_end_of_round_goals,
-        p1_eggs,
-        p1_food_on_cards,
-        p1_tucked_cards,
-        p1_nectar,
-        p2_id,
-        p2_bird_points,
-        p2_bonus_cards,
-        p2_end_of_round_goals,
-        p2_eggs,
-        p2_food_on_cards,
-        p2_tucked_cards,
-        p2_nectar  
-    } = CreateGame.parse({
+    } = AddGame.parse({
         date: formData.get('gamedate'),
         time: formData.get('gametime'),
         european_expansion: safeParseBool(formData.get('europeanexp')),
         oceania_expansion: safeParseBool(formData.get('oceaniaexp')),
         asian_expansion: safeParseBool(formData.get('asianexp')),
-        p1_id: formData.get('p1name'),
-        p1_bird_points: safeParseInt(formData.get('p1birdpoints')),
-        p1_bonus_cards: safeParseInt(formData.get('p1bonuscards')),
-        p1_end_of_round_goals: safeParseInt(formData.get('p1eorgoals')),
-        p1_eggs: safeParseInt(formData.get('p1eggs')),
-        p1_food_on_cards: safeParseInt(formData.get('p1foodoncard')),
-        p1_tucked_cards: safeParseInt(formData.get('p1tuckedcards')),
-        p1_nectar: safeParseInt(formData.get('p1nectar')),
-        p2_id: formData.get('p2name'),
-        p2_bird_points: safeParseInt(formData.get('p2birdpoints')),
-        p2_bonus_cards: safeParseInt(formData.get('p2bonuscards')),
-        p2_end_of_round_goals: safeParseInt(formData.get('p2eorgoals')),
-        p2_eggs: safeParseInt(formData.get('p2eggs')),
-        p2_food_on_cards: safeParseInt(formData.get('p2foodoncard')),
-        p2_tucked_cards: safeParseInt(formData.get('p2tuckedcards')),
-        p2_nectar: safeParseInt(formData.get('p2nectar')),
     });
-    console.log('Euro Expansion', formData.get('europeanexp'));
 
     await sql`
     INSERT INTO games (date, time, european_expansion, oceania_expansion, asian_expansion)
     VALUES (${date}, ${time}, ${european_expansion}, ${oceania_expansion}, ${asian_expansion})
     `;
 
-    await sql`
-    INSERT INTO scores (game_id,player_id,bird_points,bonus_cards,end_of_round_goals,eggs,food_on_cards,tucked_cards,nectar)
-    VALUES ((SELECT MAX(id) FROM games), ${p1_id}, ${p1_bird_points}, ${p1_bonus_cards}, ${p1_end_of_round_goals}, ${p1_eggs}, ${p1_food_on_cards}, ${p1_tucked_cards}, ${p1_nectar})
-    `;
+    const ScoreDataSchema = z.object({
+        id: z.string(),
+        bird_points: z.number(),
+        bonus_cards: z.number(),
+        end_of_round_goals: z.number(),
+        eggs: z.number(),
+        food_on_cards: z.number(),
+        tucked_cards: z.number(),
+        nectar: z.number(),
+    });
 
-    await sql`
-    INSERT INTO scores (game_id,player_id,bird_points,bonus_cards,end_of_round_goals,eggs,food_on_cards,tucked_cards,nectar)
-    VALUES ((SELECT MAX(id) FROM games), ${p2_id}, ${p2_bird_points}, ${p2_bonus_cards}, ${p2_end_of_round_goals}, ${p2_eggs}, ${p2_food_on_cards}, ${p2_tucked_cards}, ${p2_nectar})
-    `;
+    for (let i = 1; i <= numPlayers; i++) {
+        const {
+            id,
+            bird_points,
+            bonus_cards,
+            end_of_round_goals,
+            eggs,
+            food_on_cards,
+            tucked_cards,
+            nectar,
+        } = ScoreDataSchema.parse({
+            id: formData.get(`p${i}name`),
+            bird_points: safeParseInt(formData.get(`p${i}birdpoints`)),
+            bonus_cards: safeParseInt(formData.get(`p${i}bonuscards`)),
+            end_of_round_goals: safeParseInt(formData.get(`p${i}eorgoals`)),
+            eggs: safeParseInt(formData.get(`p${i}eggs`)),
+            food_on_cards: safeParseInt(formData.get(`p${i}foodoncard`)),
+            tucked_cards: safeParseInt(formData.get(`p${i}tuckedcards`)),
+            nectar: safeParseInt(formData.get(`p${i}nectar`)),
+        });
+
+        await sql`
+        INSERT INTO scores (game_id,player_id,bird_points,bonus_cards,end_of_round_goals,eggs,food_on_cards,tucked_cards,nectar)
+        VALUES ((SELECT MAX(id) FROM games), ${id}, ${bird_points}, ${bonus_cards}, ${end_of_round_goals}, ${eggs}, ${food_on_cards}, ${tucked_cards}, ${nectar})
+        `;
+    
+    };
 
     revalidatePath('/');
     redirect('/');
--- wingspanscorecard/src/app/ui/add-game.tsx
+++ wingspanscorecard/src/app/ui/add-game.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { Player } from '@/app/lib/definitions';
-import { createGame } from '@/app/lib/server-uploads';
+import { addGame } from '@/app/lib/server-uploads';
 import ScoreColumn from '@/app/ui/score-column';
 
 export default function Form({ players }: { players: Player[] }) {
@@ -16,7 +16,7 @@ export default function Form({ players }: { players: Player[] }) {
     };
 
     return (
-        <form action={createGame}>
+        <form action={addGame}>
             <div className="flex flex-col gap-4 w-full">
                 <label>
                     Number of Players
