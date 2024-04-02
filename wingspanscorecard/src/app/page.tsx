ba4e0feb992875c9dc2f15d40d11f70d308c366a
amoore <amoore@brocksolutions.com>
2024-04-02 18:47:58 +0100

    Added special styling for winner in game cards

--- wingspanscorecard/src/app/page.tsx
+++ wingspanscorecard/src/app/page.tsx
@@ -7,21 +7,15 @@ import { fetchGameData, fetchScores } from "@/app/lib/server-fetches";
 export default async function Page() {
     const numGames = Array.from({ length: 5 }, (_, index) => index);
     const gameData = await fetchGameData();
-    const gameid = gameData[1].id;
-    const scoresFetched = await fetchScores(gameid);
     const scores = await Promise.all(gameData.map(game => fetchScores(game.id)));
-
-    // console.log(gameData);
-    // console.log(scores);
 
     return (
         <>
             <MenuBar />
             <div className="flex flex-col w-full max-w-7xl p-7 m-auto">
                 {numGames.map((game) => (
-                    <GameCard key={game} scores={scores[game]} gameData={gameData[game]} />
+                    <GameCard key={game} winnerScore={scores[game][0]} scores={scores[game].slice(1, 5)} gameData={gameData[game]} />
                 ))}
-                {/* <GameCard scores={scoresFetched} gameData={gameData[0]} /> */}
             </div>
         </>
     );
--- wingspanscorecard/src/app/ui/recent-games/game-card.tsx
+++ wingspanscorecard/src/app/ui/recent-games/game-card.tsx
@@ -3,15 +3,19 @@ import CalendarIcon from "@/app/ui/icons/calendar-icon";
 import HashtagIcon from "@/app/ui/icons/hashtag-icon";
 
 
-export default function GameCard( { scores, gameData }: { scores: Array<PlayerScore>; gameData: Game; } ) {
+export default function GameCard( { winnerScore, scores, gameData }: { winnerScore: PlayerScore; scores: Array<PlayerScore>; gameData: Game; } ) {
     return (
         <div className="w-full bg-seagull-700 rounded-2xl mt-1 mb-1">
             <div className="w-full pt-4 pb-4 pl-8 pr-8">
                 <div className="width-full">
+                    <div className="flex justify-between w-full">
+                        <div className="text-white text-3xl font-semibold">{winnerScore.player}</div>
+                        <div className="text-white text-3xl font-bold">{winnerScore.score}</div>
+                    </div>
                     {scores.map((score) => (
                         <div key={score.id} className="flex justify-between w-full">
-                            <div className="text-white text-xl font-semibold">{score.player}</div>
-                            <div className="text-white text-xl font-bold">{score.score}</div>
+                            <div className="text-white text-xl font-medium">{score.player}</div>
+                            <div className="text-white text-xl font-semibold">{score.score}</div>
                         </div>
                     ))}
                     <div className="flex justify-end gap-3 items-center">
