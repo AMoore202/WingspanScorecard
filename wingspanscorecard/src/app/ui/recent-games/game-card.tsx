3c0c9b5b0107f3354407d297fc4590a2a1d16ca2
amoore <amoore@brocksolutions.com>
2024-04-03 18:42:14 +0100

    Added player rank into game cards

--- wingspanscorecard/src/app/ui/recent-games/game-card.tsx
+++ wingspanscorecard/src/app/ui/recent-games/game-card.tsx
@@ -1,21 +1,31 @@
 import { PlayerScore, Game } from "@/app/lib/definitions";
 import CalendarIcon from "@/app/ui/icons/calendar-icon";
 import HashtagIcon from "@/app/ui/icons/hashtag-icon";
+import CrownIcon from "@/app/ui/icons/crown-icon";
 
 
 export default function GameCard( { winnerScore, scores, gameData }: { winnerScore: PlayerScore; scores: Array<PlayerScore>; gameData: Game; } ) {
+    const scoresLength = Array.from({ length: scores.length}, (_, i) => i + 1);
+    console.log(scoresLength);
+    
     return (
         <div className="w-full bg-seagull-700 rounded-2xl mt-1 mb-1">
             <div className="w-full pt-4 pb-4 pl-8 pr-8">
-                <div className="width-full">
+                <div className="flex flex-col width-full gap-1">
                     <div className="flex justify-between w-full">
-                        <div className="text-white text-3xl font-semibold">{winnerScore.player}</div>
+                        <div className="flex gap-2 items-center">
+                            <CrownIcon className="text-white" />
+                            <div className="text-white text-3xl font-semibold">{winnerScore.player}</div>                            
+                        </div> 
                         <div className="text-white text-3xl font-bold">{winnerScore.score}</div>
                     </div>
-                    {scores.map((score) => (
-                        <div key={score.id} className="flex justify-between w-full">
-                            <div className="text-white text-xl font-medium">{score.player}</div>
-                            <div className="text-white text-xl font-semibold">{score.score}</div>
+                    {scoresLength.map((i) => (
+                        <div key={i} className="flex justify-between w-full">
+                            <div className="flex gap-2 items-center">
+                                <div className="w-7 text-white text-xl font-medium text-center">{i + 1}</div>
+                                <div className="text-white text-xl font-medium">{scores[i - 1].player}</div>
+                            </div>
+                            <div className="text-white text-xl font-semibold">{scores[i - 1].score}</div>
                         </div>
                     ))}
                     <div className="flex justify-end gap-3 items-center">
