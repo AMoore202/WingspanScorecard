a6ed1b5656f066cfc207ee1462e2973a602fef2c
Alexander Moore <alexandermoore202@gmail.com>
2025-06-15 20:25:02 -0400

    Updating base variables

--- wingspanscorecard/tailwind.config.ts
+++ wingspanscorecard/tailwind.config.ts
@@ -11,66 +11,57 @@ const config: Config = {
   	extend: {
   		colors: {
   			seagull: {
-  				'50': '#f1f9fe',
-  				'100': '#e2f3fc',
-  				'200': '#bfe5f8',
-  				'300': '#75cbf0',
-  				'400': '#47bbe9',
-  				'500': '#1fa2d8',
-  				'600': '#1283b7',
-  				'700': '#0f6895',
-  				'800': '#11587b',
-  				'900': '#144a66',
-  				'950': '#0d2f44'
+  				'50': '#E9F6FC',
+  				'100': '#CEECF8',
+  				'200': '#9DD8F1',
+  				'300': '#70C7EB',
+  				'400': '#44B5E4',
+  				'500': '#1FA2D8',
+  				'600': '#187FA9',
+  				'700': '#125E7D',
+  				'800': '#0C3C50',
+  				'900': '#061E28',
+  				'950': '#031116'
   			},
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
+			sirocco: {
+  				'0': '#FFFFFF',
+				'50': '#F1F3F4',
+  				'100': '#E3E7E8',
+  				'200': '#C7CED1',
+  				'300': '#ABB6BA',
+  				'400': '#8F9DA3',
+  				'500': '#73858C',
+  				'600': '#5C6A70',
+  				'700': '#455054',
+  				'800': '#2E3538',
+  				'900': '#171B1C',
+  				'950': '#0B0D0E',
+				'1000': '#000000',
   			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
+  			surface: {
+				DEFAULT: 'hsl(var(--bg-default))',
+				card: 'hsl(var(--bg-card))',
+				button_primary_start: 'hsl(var(--bg-button-primary-start))',
+				button_primary_end: 'hsl(var(--bg-button-primary-end))',
+				button_secondary_start: 'hsl(var(--bg-button-secondary-start))',
+				button_secondary_end: 'hsl(var(--bg-button-secondary-end))',
+			},
+			foreground: {
+				DEFAULT: 'hsl(var(--fg-default))',
+				accent: 'hsl(var(--fg-accent))',
+				button_primary: 'hsl(var(--fg-button-primary))',
+				button_primary_subtle: 'hsl(var(--fg-button-primary-subtle))',
+				button_secondary: 'hsl(var(--fg-button-secondary))',
+			},
   		},
   		boxShadow: {
-  			menu: '0 4px 4px 0px rgba(13,47, 68, 0.25)'
+  			card: 'var(--shadow-card)',
+			button_card: 'var(--shadow-button-card)',
+			button_secondary: 'var(--shadow-button-secondary)',
   		},
   		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
+  			card: 'var(--radius-card)',
+			button: 'var(--radius-button)',
   		}
   	}
   },
